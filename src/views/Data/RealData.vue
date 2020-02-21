119ce1e95a009a92201bba6b4f6a1f98d1783ff4
admin <836237106@qq.com>
2020-02-21 17:36:28 +0800

    系统日志更新

--- src/views/Data/RealData.vue
+++ src/views/Data/RealData.vue
@@ -8,23 +8,70 @@
       <el-breadcrumb-item>实时数据</el-breadcrumb-item>
     </el-breadcrumb>
     <h1>{{ msg }}</h1>
+    <div id="main" style="width: 600px;height: 400px;"></div>
   </div>
 </template>
 <script>
+import echarts from 'echarts'
 export default {
   data () {
     return {
-      msg: '实时数据'
+      msg: "实时数据",
+      charts: '',
+      opinionData: ["3", "2", "4", "4", "5"]
     }
   },
   methods: {
+    drawLine (id) {
+      this.charts = echarts.init(document.getElementById(id))
+      this.charts.setOption({
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['近七日收益']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
 
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ["1", "2", "3", "4", "5"]
+
+        },
+        yAxis: {
+          type: 'value'
+        },
+
+        series: [{
+          name: '近七日收益',
+          type: 'line',
+          stack: '总量',
+          data: this.opinionData
+        }]
+      })
+    }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      this.drawLine('main')
+    })
   }
 }
 </script>
 <style scoped>
 .bread {
   background-color: #e9eef3;
-  padding: 10px;
+  padding: 5px;
 }
 </style>
--- src/views/System/SysLog.vue
+++ src/views/System/SysLog.vue
@@ -7,14 +7,84 @@
       <el-breadcrumb-item>系统管理</el-breadcrumb-item>
       <el-breadcrumb-item>系统日志</el-breadcrumb-item>
     </el-breadcrumb>
-    <h1>{{ msg }}</h1>
+    <div class="container" style="border:0px">
+      <el-row
+        type="flex"
+        justify="center"
+        style="margin-top:-35px;min-width:950px"
+      >
+        <el-col :span="12">
+          <span>选择日期范围：</span>
+          <el-date-picker
+            v-model="value"
+            type="daterange"
+            align="right"
+            unlink-panels
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :picker-options="pickerOptions"
+          >
+          </el-date-picker>
+        </el-col>
+        <el-col :span="7">
+          <el-input
+            clearable
+            placeholder="请输入内容"
+            v-model="input"
+            class="inline-input"
+          >
+            <el-button slot="append" icon="el-icon-search">搜日志</el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="5"> </el-col>
+      </el-row>
+      <el-input
+        type="textarea"
+        show-word-limit
+        rows="10"
+        placeholder="27日，温湿度正常，需要注意下午下雨"
+        v-model="textarea"
+      >
+      </el-input>
+      <el-button type="success" round>提交</el-button>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      msg: '系统日志'
+      msg: '系统日志',
+      input: '',
+      textarea: '',
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
+        }]      },
+      value: ''
     }
   },
   methods: {
@@ -25,6 +95,6 @@ export default {
 <style scoped>
 .bread {
   background-color: #e9eef3;
-  padding: 10px;
+  padding: 5px;
 }
 </style>
