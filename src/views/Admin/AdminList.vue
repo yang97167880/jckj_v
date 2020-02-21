44cbf2d497a06976e8c11994787c8c0826e25307
DELL <642798471@qq.com>
2020-02-21 19:41:02 +0800

    管理员列表和总视图

--- src/views/Admin/AdminList.vue
+++ src/views/Admin/AdminList.vue
@@ -2,25 +2,112 @@
   <div>
     <el-breadcrumb separator-class="el-icon-arrow-right" class="bread">
       <el-breadcrumb-item :to="{ path: '/top' }">
-        <i class="el-icon-s-home"></i>首页</el-breadcrumb-item
-      >
+        <i class="el-icon-s-home"></i>首页
+      </el-breadcrumb-item>
       <el-breadcrumb-item>管理员管理</el-breadcrumb-item>
       <el-breadcrumb-item>管理员列表</el-breadcrumb-item>
     </el-breadcrumb>
     <h1>{{ msg }}</h1>
+    <template>
+      <div style="text-align: center;">
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          tooltip-effect="dark"
+          border
+          style="width: 95%;
+          margin: auto"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column :render-header="renderHeader">
+          <el-table-column  label="管理员列表">
+            <el-table-column type="selection" width="55"></el-table-column>
+            <el-table-column prop="ID" label="ID"></el-table-column>
+            <el-table-column prop="name" label="登录名"></el-table-column>
+            <el-table-column prop="charactor" label="角色"></el-table-column>
+            <el-table-column prop="time" label="加入时间"></el-table-column>
+            <el-table-column prop="judgement" label="是否已启用">
+              <template>
+                <el-button
+                   size="mini"
+                   type="success" round
+                 @click="judegment(scope.$index, scope.row)"
+                >启用</el-button>
+              </template>
+            </el-table-column>
+            <el-table-column prop="operation" label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  type="info"
+                  plain
+                  @click="handleStop(scope.$index, scope.row)"
+                >停用</el-button>
+                <el-button plain size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  plain
+                  @click="handleDelete(scope.$index, scope.row)"
+                >删除</el-button>
+              </template>
+              </el-table-column>
+            </el-table-column>
+          </el-table-column>
+        </el-table>
+      </div>
+    </template>
+  </div>
+</template>
+
   </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
-      msg: '管理员列表'
-    }
+      msg: "管理员列表",
+      tableData: [
+        {
+          ID: "1",
+          name: "admin",
+          charactor: "超级管理员",
+          time: "2020.2.21",
+         
+        },
+        {
+          ID: "1",
+          name: "admin",
+          charactor: "超级管理员",
+          time: "2020.2.21",
+         
+        },
+      
+      ],
+      renderHeader() {
+        return (
+          <div>
+            <el-button type="primary" icon="el-icon-circle-plus">添加管理员</el-button>
+          </div>
+        );
+      }
+    };
   },
   methods: {
-
+    handleStop(index, row) {
+      console.log(index, row);
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    judegment(index, row){
+       console.log(index, row);
+    }
   }
-}
+};
 </script>
 <style scoped>
 .bread {
--- src/views/Data/TotalView.vue
+++ src/views/Data/TotalView.vue
@@ -8,8 +8,21 @@
       <el-breadcrumb-item>总试图</el-breadcrumb-item>
     </el-breadcrumb>
     <h1>{{ msg }}</h1>
+    <div class="ny">
+    <div class="wz">
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;系统分为数据采集端、数据处理端以及用户端，整体的框架图如下图所示。</p>
+        <img src="../../assets/img/zst.jpg" >
+    </div>
+    <div >
+      <p>为适应现代化工程监测的需要，国内外的基坑工程发展方向逐渐想智能化监测的方向发展，可以降低对人工采集监测数据的依赖程度，表现出自动化、远程监测、自动预警等特点。</p>
+    </div>
+
+</div>
   </div>
+
 </template>
+
+
 <script>
 export default {
   data () {
@@ -27,4 +40,10 @@ export default {
   background-color: #e9eef3;
   padding: 10px;
 }
+.ny{width: 800px;margin: 50px auto ;border: solid 1px gray;}
+.wz{width: 100%;float: left}
+.wz img{
+    display: block;
+    width: 100%;
+            }
 </style>
